43e0554105abfe98811518c070c87208686a308a
stuartjohnpage <stuartjohnpage@live.com>
2022-09-24 15:50:41 -0500

    now works for more difficult test cases

--- cashRegister.js
+++ cashRegister.js
@@ -14,7 +14,7 @@ function checkCashRegister(price, cash, cashInDrawer) {
     const tokensSmallerThanChangeDue = (changeDue, cashInDrawer) => {
         smallerTokens = [];
         cashInDrawer.forEach(currencyUnit => {
-            if (changeDue > currencyUnitDenomination[currencyUnit[0]]) {
+            if (changeDue >= currencyUnitDenomination[currencyUnit[0]]) {
                 smallerTokens.unshift(currencyUnit[0])
             }
         });
@@ -27,18 +27,26 @@ function checkCashRegister(price, cash, cashInDrawer) {
             cashTokenAmounts[element[0]] = (Math.round(element[1] / currencyUnitDenomination[element[0]]))
         });
         return cashTokenAmounts
+
+    }
+
+    const getTotalCashInDrawer = (cashInDrawer) => {
+        return parseFloat((cashInDrawer.reduce((total, element)=> {
+            return total + element[1]
+        }, 0)).toFixed(2))
     }
 
     const calculateChangeTokens = (smallerTokensThanChange, changeDue, currentCashTokenAmounts) => {
         let changeRemaining = changeDue.valueOf()
         const changeTokens = {}
+        let sanityCheck = 100000
+        console.log(smallerTokensThanChange, changeDue, currentCashTokenAmounts)
         while(changeRemaining != 0){
             for (const token of smallerTokensThanChange) {
-                console.log(currentCashTokenAmounts[token], 'here')
                 if (currencyUnitDenomination[token] <= changeRemaining && 
                 changeRemaining - currencyUnitDenomination[token] >= 0 &&
                 currentCashTokenAmounts[token] > 0) {
-                        changeRemaining -= currencyUnitDenomination[token]
+                        changeRemaining = parseFloat((changeRemaining - currencyUnitDenomination[token]).toFixed(2))
                         currentCashTokenAmounts[token] -= 1
                         if(changeTokens[token]){
                             changeTokens[token] += currencyUnitDenomination[token]
@@ -47,58 +55,44 @@ function checkCashRegister(price, cash, cashInDrawer) {
                         }
                         break
                 }
+                if(sanityCheck == 0){
+                    return false
+                }
+                sanityCheck -= 1
             }
         }
         const arrayChangeTokens = Object.entries(changeTokens)
-        console.log(arrayChangeTokens)
         return arrayChangeTokens
     }
 
     const calculateChangeDue = (price, cash) => {
-        return cash - price
+        return price - cash
     }
-    const currentCashInDrawer = Object.fromEntries(cashInDrawer)
+    // begin calculation
+    const changeDue = parseFloat(calculateChangeDue(price, cash).toFixed(2))
+    const totalCashInDrawer = parseFloat(getTotalCashInDrawer(cashInDrawer).toFixed(2))
+    if (totalCashInDrawer < changeDue) {
+        return { status: "INSUFFICIENT_FUNDS", change: [] }
+    } 
+    if (changeDue === totalCashInDrawer) {
+        return { status: "CLOSED", change: cashInDrawer }
+    }
+
     const currentCashTokenAmounts = numberOfCashTokens(cashInDrawer)
-    const changeDue = calculateChangeDue(price, cash)
     const smallerTokensThanChange = tokensSmallerThanChangeDue(changeDue, cashInDrawer)
     const changeTokens = calculateChangeTokens(smallerTokensThanChange, changeDue, currentCashTokenAmounts)
-    const canReturnExact = true
-    // console.log(
-    //     '\n cashTokenAmounts :', cashTokenAmounts, 
-    //     '\n changeDue : ', changeDue, 
-    //     '\n cashInDrawer: ', cashInDrawer,
-    //     '\n smallerTokensThanChange: ', smallerTokensThanChange
-    // )
-    
-
-    // condition 1
-    if (cashInDrawer < changeDue || !canReturnExact) {
-        return { status: "INSUFFICIENT_FUNDS", change: [] }
-        // condition 2
-    } else if (changeDue === cashInDrawer) {
-        return { status: "CLOSED", change: cashInDrawer }
-        // condition 3
-    } else {
-        return { status: "OPEN", change: [] }
+    if(!changeTokens){
+        return {status: "INSUFFICIENT_FUNDS", change: []}
     }
+
+    return { status: "OPEN", change: changeTokens }
+    
 }
 
-const price = 19.5
-const cash = 20
-const cashInDrawer = [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]
+const cash = 13.76
+const price = 20
+const cid = [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]
+console.log(checkCashRegister(price, cash, cid));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+// [["PENNY", 0], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 2], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]
+// [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]
